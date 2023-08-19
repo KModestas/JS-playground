78e7ef909f6186ee9600dafb0305f6e697b1bd67
Modestas <modestask97@gmail.com>
2023-08-19 16:15:40 +0100

    add multiple node event loop examples

---
+++ eventloop.js
@@ -0,0 +1,62 @@
+// the main module (this script) will first be executed synchronously from top to bottom and only then when the callstack is empty, the event loop will begin checking each queue and processing each callback)
+const fs = require('fs');
+
+// Macro tasks:
+const timersQueue = []; // setTimeout, setInterval
+const checkQueue = []; // setImmediate callbacks 
+const pollQueue = []; // I/O related callbacks (Most of your Node.js events e.g. file reads, req handler cb's)
+const closeEventsQueue = []; // callbacks from close / exit events
+// Micro tasks:
+const microtasksQueue = []; // Promise callbacks
+const nextTickQueue = [] // process.nextTick() callbacks
+
+// Any Active handlers or requests would be tracked by node.js and aslong as there are any, node will not terminate the process, (until they are closed)
+const activeHandlers = [] // Open sockets, listening to servers or timers that are scheduled to run but their time hasn't come yet.
+const activeRequests = [] // Async operations such as reading from a file which are in progress and haven't completed yet.
+
+// *** MICROTASKS PROCESSED ***
+
+process.nextTick(() => {
+  console.log('1. Next Tick callback');
+  nextTickQueue.push()
+});
+
+Promise.resolve().then(() => {
+    console.log('2. Promise callback');
+    microtasksQueue.push();
+});
+
+// *** EVENT LOOP PHASES BEGIN ***
+
+// [Timers Phase]
+setTimeout(() => {
+    console.log('3. Timer callback');
+    timersQueue.push();
+}, 0);
+
+// [Poll Phase] 
+// Initially when poll queue is checked, it will be empty even if callback has completed, the event loop will Poll the system to check if the callback has completed, add it to the queue and then move on to the [Check Phase], before executing in the next [Poll Phase]
+fs.readFile(__filename, () => {
+    console.log('5. Poll (File read) callback');
+    pollQueue.push();
+});
+
+// [Check Phase]
+// NOTE: setImmediate will always run before setTimeout 0 if inside another callback.
+setImmediate(() => {
+    console.log('4. Check (setImmediate) callback');
+    checkQueue.push();
+});
+
+
+// [Close events Phase] 
+process.on('exit', () => {
+    console.log('6. Close event callback (process exit)');
+    closeEventsQueue.push();
+});
+
+// End of Script: now go over it again from top to bottom keeping in mind that the callstack is empty but event queues are now populated.
+
+// check whats keeping your event loop from exiting the process:
+// process._getActiveHandles()
+// process._getActiveRequests()
---
+++ eventloop3.js
@@ -0,0 +1,37 @@
+// This example is complicated! The output will change if you run it a few times but it's a good exercise to go through the flow
+// Read each step in the numbered order (This does NOT necessarily denote the order of execution)
+
+// NOTE: "Poll - setTimeout" denotes that it's triggered in [Poll phase] but ofcourse its always EXECUTED in the [Timer Phase] etc.
+
+// IMPORTANT: only run this one at a time and go through the flow, if you have too many logs in the console you will get distracted and confused.
+const fs = require('fs')
+
+// 1. [Timer phase] - processed first, however if race condition happens then setImmediate below may be logged first
+setTimeout(() => { 
+    console.log('setTimeout');
+}, 0);
+
+// 2. [Poll Phase] - callback is added to the queue but doesn't run until next cycle
+fs.readFile(__filename, () => {
+// 6. this I/O call back is run
+  console.log('Poll - readFile')
+  // 7. setImmediate will always run next [Check Phase] 
+  setImmediate(() => console.log('Poll - setImmediate'));
+  // 8. This is the only thing left to execute while the Promise is waiting so it will always log second to last
+ setTimeout(() =>  console.log('Poll - setTimeout'), 0);
+});
+
+// 3. [Check Phase] - callback is executed next:
+setImmediate(async () => {
+  console.log('setImmediate');
+  
+  // 4. After [Check] is [Timer] phase again but due to race condition, this callback might not run next but instead After [Poll]
+  setTimeout(() => console.log('Check - setTimeout'), 0);
+  setImmediate(() => console.log('Check - setImmediate'));
+
+  // 5. Promise is executed which schedules the setTimeout, this entire function will be popped off the execution stack and will resume when then promise resolves inside the setTimeout callback
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  // 9. After 1 second has elapsed this finally gets executed:
+  setImmediate(() => console.log('Check - setImmediate'));
+});
